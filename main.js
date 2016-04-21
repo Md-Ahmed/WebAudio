69e185fcf888dac773c4936419c744607fcfb25c
Ahmed <mohd-ahmed@outlook.com>
2016-04-22 03:08:54 +0530

    Added Play/Pause Button

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
 var audioContext = new (window.AudioContext || window.webkitAudioContext)();
-var oscillator = audioContext.createOscillator();
-oscillator.type = 'sine';
-oscillator.frequencyValue = 3500;
-oscillator.start();
-oscillator.connect(audioContext.destination);
+var oscillator;
+isPlaying = false;
+function play(){
+    oscillator = audioContext.createOscillator();
+    oscillator.type = 'sine';
+    oscillator.frequencyValue = 3500;
+    oscillator.connect(audioContext.destination);
+    oscillator.start();
+}
+function stop() {
+    oscillator.disconnect();
+}
+function togglePlay() {
+    if(isPlaying){
+        stop();
+        isPlaying = false;
+    }
+    else{
+        play();
+        isPlaying = true;
+    }
+}
