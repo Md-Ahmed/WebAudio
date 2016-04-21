e1db8f2a774b5ee5ea5a99f292fb28150cd98a1c
Ahmed <mohd-ahmed@outlook.com>
2016-04-22 02:37:40 +0530

    Added basic audio to main.js

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-console.log("Test");
+var audioContext = new (window.AudioContext || window.webkitAudioContext)();
+var oscillator = audioContext.createOscillator();
+oscillator.type = 'sine';
+oscillator.frequencyValue = 3500;
+oscillator.start();
+oscillator.connect(AudioContext.destination);
