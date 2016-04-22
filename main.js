515a11d645b2acb961dbd42fdd8f0999b21ab926
Ahmed <mohd-ahmed@outlook.com>
2016-04-22 20:03:43 +0530

    Added a test canvas

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+var canvas = document.getElementById('canvas');
+console.log(canvas);
+var context = canvas.getContext("2d");
+console.log(context);
+context.fillStyle = "green";
+console.log(context.fillStyle);
+context.fillRect(10, 10, 100, 100);
 var audioContext = new (window.AudioContext || window.webkitAudioContext)();
 var oscillator;
 isPlaying = false;
