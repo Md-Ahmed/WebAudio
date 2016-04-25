7775cb56c43c22eece9f7e89e96d0403aca86bd8
Ahmed <mohd-ahmed@outlook.com>
2016-04-25 15:24:59 +0530

    Cleaned main.js

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-var x = 0;
-var y = 0.00;
+var angle = 0;
 
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext("2d");
+width = canvas.width;
+height = canvas.height;
+context.fillStyle = "green";
 render();
 
 var audioContext = new (window.AudioContext || window.webkitAudioContext)();
@@ -33,28 +35,11 @@ function togglePlay() {
         isPlaying = true;
     }
 }
-/*function render(){
-    context.fillStyle = "green";
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    for(var i = 0; i < canvas.width; i++){
-        context.fillRect(x, Math.sin((180/Math.PI)*y)*20 + (canvas.height/2), 2, 2);
-        x++;
-        y += .002;
-    }
-    x++;
-    requestAnimationFrame(render);
-}*/
 
 function render(){
-    context.fillStyle = "green";
-    context.clearRect(0, 0, canvas.width, canvas.height);
-    for(var i = 0; i < canvas.width; i++){
-        context.fillRect(x, Math.sin((180/Math.PI)*y)*20 + (canvas.height/2), 2, 2);
-        x += 1;
-        y += .002;
-    }
-    if (x > canvas.width){
-        x = 0;
+    context.clearRect(0, 0, width, height);
+    for(var i = 0; i < width; i++){
+        context.fillRect(i*5, Math.sin(i)*5 + height/2, 2, 2);
     }
     requestAnimationFrame(render);
 }
